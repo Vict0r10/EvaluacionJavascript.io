e249869e8058b8051a7d5458b0d1d0a2c7606376
Vict0r10 <129803971+Vict0r10@users.noreply.github.com>
2023-06-09 19:42:31 +0000

    commitc

--- script.js
+++ script.js
@@ -1,20 +1,59 @@
 function acc1() {
-  var nom1 = document.getElementById("n1").value;
-  var nom2 = document.getElementById("n2").value;
-  var ed1 = document.getElementById("e1").value;
-  var ed2 = document.getElementById("e2").value;
+  var nom = document.getElementById("name").value;
+  var edad = document.getElementById("age").value;
+  var telefono = document.getElementById("phone").value;
+  var mail = document.getElementById("email").value;
+  var mod = document.getElementById("model").value;
+  var efectivo= parseInt(document.getElementById("ef").value);
+  var seg = document.getElementById("seguro").value;
 
-  var resultado = parseInt(ed1)+parseInt(ed2);
 
-  document.getElementById("resultado").innerHTML= " "+ resultado;
+  var precio
+  if (isNaN(edad)) {
+
+    document.getElementById("age").value= "";
+    alert("La edad debe ser un numero");
+  } 
+  if (isNaN(telefono)) {
+    document.getElementById("phone").value= "";
+    alert("El telefono debe ser un numero");
+  }
+  
+  if (efectivo === '1') {
+    precio = 100000 * 0.95;
+  } else {
+    precio = 100000;
+  }
+  if (seguro === "Si") {
+  } else {
+    seg="No";
+  }
+
+  document.getElementById("resultado").innerHTML="Recibo:<br>"+"<br>Nombre:"+ nom + "<br>Telefono:" + telefono+ "<br>Email: "+mail+"<br>Codigo Modelo:"+mod + "<br>Precio: $"+precio +"<br>Seguro: "+ seg;
+
+
+}
+
+function inter() {
+  window.location.href = "interacion.html";
+
+}
+
+function m_page() {
+  window.location.href = "index.html";
+
 }
 
 function limpiar() {
 
-      document.getElementById("n1").value= "";
-      document.getElementById("n2").value= "";
-      document.getElementById("e1").value= "";
-      document.getElementById("e2").value= "";
+      document.getElementById("name").value= "";
+      document.getElementById("age").value= "";
+      document.getElementById("phone").value= "";
+      document.getElementById("email").value= "";
+      document.getElementById("model").value= "";
+
+
+
       document.getElementById("resultado").innerText= " ";
 
 
